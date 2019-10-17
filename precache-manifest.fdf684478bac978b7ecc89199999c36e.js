77299c342af9f1643b8e37ce612deea929a27f35
Yongshi Luo <luo00046@algonquinlive.com>
2019-10-16 23:03:00 -0400

    Updates

---
+++ precache-manifest.fdf684478bac978b7ecc89199999c36e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9557f9f19b592862b308aca72149496b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d9c4ebd0cbe116c7dc2",
+    "url": "/monsters-rolodex/static/css/main.ee83ee84.chunk.css"
+  },
+  {
+    "revision": "102a9b24fd9babe5b595",
+    "url": "/monsters-rolodex/static/js/2.a393edc3.chunk.js"
+  },
+  {
+    "revision": "2d9c4ebd0cbe116c7dc2",
+    "url": "/monsters-rolodex/static/js/main.764df922.chunk.js"
+  },
+  {
+    "revision": "29b8639263678424eef0",
+    "url": "/monsters-rolodex/static/js/runtime-main.7352cc78.js"
+  }
+]);
